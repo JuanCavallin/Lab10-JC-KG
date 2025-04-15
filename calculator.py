60ecdab8b0675e2ad069e1e01419dba9a4e8d49a
JuanCavallin <61669937+JuanCavallin@users.noreply.github.com>
2025-04-14 23:50:17 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,6 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
+#https://github.com/JuanCavallin/Lab10-JC-KG
+#Partner 1: Juan Cavallin
+#Partner 2: Kieran Galveston
 import math
 
 def add(a, b):
